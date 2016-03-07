b28965555c5d5d6ae8070f75e683de117b4e3c1c
Kirill Fomichev <fanatid@ya.ru>
2016-03-07 21:53:19 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# npm-rdtree-markdown
+
+[![NPM Package](https://img.shields.io/npm/v/npm-rdtree-markdown.svg?style=flat-square)](https://www.npmjs.org/package/npm-rdtree-markdown)
+[![Build Status](https://img.shields.io/travis/fanatid/npm-rdtree-markdown.svg?branch=master&style=flat-square)](https://travis-ci.org/fanatid/npm-rdtree-markdown)
+[![Dependency status](https://img.shields.io/david/fanatid/npm-rdtree-markdown.svg?style=flat-square)](https://david-dm.org/fanatid/npm-rdtree-markdown#info=dependencies)
+
+[![js-standard-style](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
+
+## Installation
+
+```shell
+$ npm i https://github.com/fanatid/npm-rdtree-markdown -g
+$ npm-rdtree-markdown crypto-browserify
+...
+$ cd work-directory/crypto-browserify
+$ npm-rdtree-markdown
+... (will automatically use package.json from current directory)
+```
+
+## License
+
+MIT
---
+++ npm-rdtree-markdown.js
@@ -0,0 +1,89 @@
+#!/usr/bin/env node
+'use strict'
+var http = require('http')
+var fs = require('fs')
+var path = require('path')
+var co = require('co')
+var semver = require('semver')
+
+function getPackageInfo (name) {
+  return new Promise((resolve, reject) => {
+    console.log(`Request http://registry.npmjs.org/${name}`)
+    let req = http.request({ host: 'registry.npmjs.org', path: `/${name}` }, (res) => {
+      let body = ''
+      res.on('error', (err) => reject(err))
+      res.on('data', (data) => { body += data })
+      res.on('end', () => {
+        try { resolve(JSON.parse(body)) } catch (err) { reject(err) }
+      })
+    })
+    req.on('error', (err) => reject(err))
+    req.end()
+  })
+}
+
+co.wrap(function * () {
+  let packageInfoRoot = yield co.wrap(function * () {
+    if (process.argv[2]) return yield getPackageInfo(process.argv[2])
+    var fullPath = path.join(process.cwd(), 'package.json')
+    var content = fs.readFileSync(fullPath, { encoding: 'utf-8' })
+    return JSON.parse(content)
+  })()
+
+  let packages = {}
+  let queue = []
+
+  function processInfo (item, info) {
+    let versions = Object.keys(info.versions).sort(semver.compareLoose)
+    let latestVersion = versions[versions.length - 1]
+    let latestDependencies = info.versions[latestVersion].dependencies || {}
+    let dependencies = Object.keys(latestDependencies).sort().map((name) => {
+      return { name, deep: false }
+    })
+
+    let githubURL = (function () {
+      if (info.homepage) return info.homepage
+      if (info.repository) return info.repository.url ? info.repository.url : info.repository
+      return ''
+    })().split('/').slice(-2).join('/').split('#')[0]
+
+    packages[info.name] = { dependencies, github: githubURL }
+    if (item.parent !== null) {
+      for (let obj of packages[item.parent].dependencies) {
+        if (obj.name === info.name) obj.deep = true
+      }
+    }
+
+    for (let item of dependencies) queue.push({ name: item.name, parent: info.name })
+  }
+
+  processInfo({ parent: null }, packageInfoRoot)
+  while (queue.length > 0) {
+    let item = queue.shift()
+    if (!packages[item.name]) processInfo(item, yield getPackageInfo(item.name))
+  }
+
+  console.log('Data collection is finished!')
+  console.log('================================================================================')
+
+  // generate dependencies tree
+  ;(function printTree (name, padding) {
+    console.log(`${padding}- [${name}](#${name.replace(/\./g, '')})`)
+    for (let item of packages[name].dependencies) {
+      if (item.deep) {
+        printTree(item.name, padding + '  ')
+      } else {
+        console.log(`${padding}  - [${item.name}](#${item.name.replace(/\./g, '')})`)
+      }
+    }
+  })(packageInfoRoot.name, '')
+
+  // generate table
+  console.log(`\n| package | npm | dependencies | github issues |\n|:-:|:-:|:-:|:-:|`)
+  Object.keys(packages).sort().forEach((name) => {
+    let info = packages[name]
+    console.log(`| <h6><a href="https://github.com/${info.github}">${name}</a></h6> | [![](https://img.shields.io/npm/v/${name}.svg?style=flat-square)](https://www.npmjs.org/package/${name}) | [![](https://img.shields.io/david/${info.github}.svg?style=flat-square)](https://david-dm.org/${info.github}#info=dependencies) | [![](https://img.shields.io/github/issues-raw/${info.github}.svg?style=flat-square)](https://github.com/${info.github}/issues) |`)
+  })
+
+  console.log('================================================================================')
+})().catch((err) => console.error(err.stack || err))
